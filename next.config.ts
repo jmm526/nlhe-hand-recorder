fa5620459826e2ad79b1ed99d50ae980916397f3
jmm526 <jakemyers2016@u.northwestern.edu>
2024-12-04 17:05:24 -0600

    .

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  ignoreBuildErrors: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  }
 };
 
 export default nextConfig;

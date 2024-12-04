ce9e1d8683b4ec21a0090a03d16f450be1b762aa
jmm526 <jakemyers2016@u.northwestern.edu>
2024-12-04 17:40:24 -0600

    transpile packages

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  
   /* config options here */
+  transpilePackages: [
+    "antd",
+    "rc-util",
+    "rc-pagination",
+    "rc-picker",
+    "rc-tree",
+    "rc-table",
+    "@ant-design/icons",
+    "@ant-design/icons-svg",
+  ],
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
 };
 
 export default nextConfig;

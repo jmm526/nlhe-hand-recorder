5bdbedd123260e2a611005246bec23061a31853b
jmm526 <jakemyers2016@u.northwestern.edu>
2024-12-06 14:16:10 -0600

    fix copy to clipboard

--- src/components/ReplayPage/HandHistoryModal.tsx
+++ src/components/ReplayPage/HandHistoryModal.tsx
@@ -11,87 +11,11 @@ import CopyToClipboardButton from "../general/CopyToClipboardButton";
 
 interface Props {
   isOpen: boolean;
-  responseData: IHandHistory | null;
+  handHistoryText: string;
   onClose: () => void;
 }
 
-const convertActionToText = (action: IAction) => {
-  let retString = `${action.position} (${action.stack_size}): ${action.action}`;
-  if (action.action !== EAction.FOLD && action.action !== EAction.CHECK) {
-    retString += ` ${!!action.amount && `(${action.amount})`}`;
-  }
-  return `${retString}\n`;
-};
-
-const generateStreetText = (
-  street: "Preflop" | "Flop" | "Turn" | "River",
-  potSize: number,
-  actions: IAction[],
-  cards?: ICard[]
-) => {
-  const playerMap = new Map<EPosition, number>();
-  let retString = `\n${street}${
-    cards
-      ? ` (${cards
-          .map((card) => `${card.value}${convertSuitToShorthand(card.suit)}`)
-          .join(", ")})`
-      : ""
-  } (Potsize: ${potSize})\n`;
-  actions.forEach((action) => {
-    if (action.action !== EAction.FOLD && action.action !== EAction.CHECK) {
-      playerMap.set(action.position, action.amount);
-    }
-    retString += convertActionToText(action);
-  });
-  const finalPotSize =
-    Array.from(playerMap.values()).reduce((a, b) => a + b, 0) + potSize;
-  return { retString, finalPotSize };
-};
-
-const convertHandHistoryToText = (handHistory: IHandHistory | null) => {
-  if (!handHistory) {
-    return "";
-  }
-  const { blinds, player, Preflop: preflop, Flop: flop, Turn: turn, River: river } = handHistory;
-  let retString = "";
-
-  retString += `Blinds: ${blinds.small_blind} / ${blinds.big_blind}\n`;
-  retString += `Hero (${player.position}): [${player.hand[0].value}${convertSuitToShorthand(
-    player.hand[0].suit
-  )}, ${player.hand[1].value}${convertSuitToShorthand(player.hand[1].suit)}]\n`;
-
-  const { retString: preflopString, finalPotSize: flopPotSize } =
-    generateStreetText("Preflop", 0, preflop);
-  retString += preflopString;
-
-  const { retString: flopString, finalPotSize: turnPotSize } =
-    generateStreetText("Flop", flopPotSize, flop.actions, flop.flop);
-  retString += flopString;
-
-  if (turn.card) {
-    const { retString: turnString, finalPotSize: riverPotSize } =
-      generateStreetText("Turn", turnPotSize, turn.actions, [turn.card]);
-    retString += turnString;
-
-    if (river.card) {
-      const { retString: riverString } = generateStreetText(
-        "River",
-        riverPotSize,
-        river.actions,
-        [river.card]
-      );
-      retString += riverString;
-    }
-  }
-
-  return retString;
-};
-
-const HandHistoryModal = ({ isOpen, responseData, onClose }: Props) => {
-  if (!responseData) {
-    return null;
-  }
-  const handHistoryText = convertHandHistoryToText(responseData);
+const HandHistoryModal = ({ isOpen, handHistoryText, onClose }: Props) => {
   return (
     <>
       <Modal
